082d9a2f8b4e451bb0eb1b2c992f12ac216033b2
Bermagambet <bermagambet.d@gmail.com>
2020-05-31 22:08:04 +0600

    AnalogueClock application

---
+++ Task5_AnalogueClock/DigitalClocks/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace DigitalClocks
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.textBox1.Location = new System.Drawing.Point(12, 266);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(485, 49);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.label1.Location = new System.Drawing.Point(4, 178);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 46);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter time:";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.button1.Location = new System.Drawing.Point(12, 335);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 40);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Show";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.button2.Location = new System.Drawing.Point(399, 335);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 40);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(552, 77);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(426, 377);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 394);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(394, 29);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "The given time format is not correct.";
+            this.label2.Visible = false;
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(7, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(264, 29);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Time format - hh:mm:ss";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(1021, 557);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Task5_AnalogueClock/DigitalClocks/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DigitalClocks
+{
+    public partial class Form1 : Form
+    {
+        Regex rgx = new Regex(@"^[0-9]{2}:[0-9]{2}:[0-9]{2}$");
+        Regex rgx2 = new Regex(@"^[0][0-9]$");
+        Timer t = new Timer();
+        int WIDTH = 300, HEIGHT = 300, secHAND = 140, minHAND = 110, hrHAND = 80;
+        // in center   
+        int cy, cx;
+        Bitmap bmp;
+        Graphics cg;
+
+        int ss = DateTime.Now.Second;
+        int mm = DateTime.Now.Minute;
+        int hh = DateTime.Now.Hour;
+
+        public Form1()
+        {
+
+            InitializeComponent();
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+        }
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            
+
+            label2.Visible = false;
+            string givenTime = textBox1.Text;
+
+            if (givenTime.Equals(""))
+            {
+                label2.Visible = true;
+                return;
+            }
+
+                if (!rgx.IsMatch(givenTime))
+            {
+                label2.Visible = true;
+                return;
+            }
+            string[] hrsMnsScnds = givenTime.Split(':');
+            for(int i = 0; i < hrsMnsScnds.Length; i++)
+            {
+                if(rgx2.IsMatch(hrsMnsScnds[i]))
+                {
+                    hrsMnsScnds[i].Remove(0, 1);
+                }
+            }
+
+            this.hh = int.Parse(hrsMnsScnds[0]);
+            this.mm = int.Parse(hrsMnsScnds[1]);
+            this.ss = int.Parse(hrsMnsScnds[2]);
+            
+            if((hh < 0 && hh > 24) || (mm < 0 && mm > 60) || (ss < 0 && ss > 60))
+            {
+                label2.Visible = true;
+                return;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(WIDTH + 1, HEIGHT + 1);
+            cx = WIDTH / 2;
+            cy = HEIGHT / 2;
+            this.BackColor = Color.White;
+            t.Interval = 1000; 
+            t.Tick += new EventHandler(this.t_Tick);
+            t.Start();
+        }
+        private void t_Tick(object sender, EventArgs e)
+        { 
+            cg = Graphics.FromImage(bmp);
+            int[] handCoord = new int[2];
+            cg.Clear(Color.White);
+            cg.DrawEllipse(new Pen(Color.Black, 6.0f), 0, 0, WIDTH, HEIGHT);
+            cg.DrawString("12", new Font("Ariel", 12), Brushes.Black, new PointF(140, 3));
+            cg.DrawString("1", new Font("Ariel", 12), Brushes.Black, new PointF(218, 22));
+            cg.DrawString("2", new Font("Ariel", 12), Brushes.Black, new PointF(263, 70));
+            cg.DrawString("3", new Font("Ariel", 12), Brushes.Black, new PointF(285, 140));
+            cg.DrawString("4", new Font("Ariel", 12), Brushes.Black, new PointF(263, 212));
+            cg.DrawString("5", new Font("Ariel", 12), Brushes.Black, new PointF(218, 259));
+            cg.DrawString("6", new Font("Ariel", 12), Brushes.Black, new PointF(142, 279));
+            cg.DrawString("7", new Font("Ariel", 12), Brushes.Black, new PointF(70, 259));
+            cg.DrawString("8", new Font("Ariel", 12), Brushes.Black, new PointF(22, 212));
+            cg.DrawString("9", new Font("Ariel", 12), Brushes.Black, new PointF(1, 140));
+            cg.DrawString("10", new Font("Ariel", 12), Brushes.Black, new PointF(22, 70));
+            cg.DrawString("11", new Font("Ariel", 12), Brushes.Black, new PointF(70, 22));
+            
+            handCoord = msCoord(ss, secHAND);
+            cg.DrawLine(new Pen(Color.Red, 2.0f), new Point(cx, cy), new Point(handCoord[0], handCoord[1]));
+            handCoord = msCoord(mm, minHAND);
+            cg.DrawLine(new Pen(Color.Black, 3.0f), new Point(cx, cy), new Point(handCoord[0], handCoord[1]));
+            handCoord = hrCoord(hh % 12, mm, hrHAND);
+            cg.DrawLine(new Pen(Color.Black, 3.0f), new Point(cx, cy), new Point(handCoord[0], handCoord[1]));  
+            pictureBox1.Image = bmp;
+            this.Text = "Analog Clock - " + hh + ":" + mm + ":" + ss;
+            cg.Dispose();
+        }
+        private int[] msCoord(int val, int hlen)
+        {
+            int[] coord = new int[2];
+            val = val * 6;
+            if (val >= 0 && val <= 100)
+            {
+                coord[0] = cx + (int)(hlen * Math.Sin(Math.PI * val / 180));
+                coord[1] = cy - (int)(hlen * Math.Cos(Math.PI * val / 180));
+            }
+            else
+            {
+                coord[0] = cx - (int)(hlen * -Math.Sin(Math.PI * val / 180));
+                coord[1] = cy - (int)(hlen * Math.Cos(Math.PI * val / 180));
+            }
+            return coord;
+        }
+        //coord for hour   
+        private int[] hrCoord(int hval, int mval, int hlen)
+        {
+            int[] coord = new int[2];
+            //each hour makes 60 degree with min making 0.5 degree   
+            int val = (int)((hval * 30) + (mval * 0.5));
+            if (val >= 0 && val <= 180)
+            {
+                coord[0] = cx + (int)(hlen * Math.Sin(Math.PI * val / 180));
+                coord[1] = cy - (int)(hlen * Math.Cos(Math.PI * val / 180));
+            }
+            else
+            {
+                coord[0] = cx - (int)(hlen * -Math.Sin(Math.PI * val / 180));
+                coord[1] = cy - (int)(hlen * Math.Cos(Math.PI * val / 180));
+            }
+            return coord;
+
+
+
+
+        }
+    }
+}
